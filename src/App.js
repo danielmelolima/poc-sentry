db3c1c70c2b9b04c6f9062ee721fb7abc2727554
Daniel Lima <daniel.lima@ifactory.com.br>
2020-08-18 10:31:38 -0300

    Adiciona erro no botão

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import * as Sentry from '@sentry/react';
 
 Sentry.init({dsn: "https://a294664c9030479390604f7e1ec41558@o435721.ingest.sentry.io/5395524"});
 
+const p = {};
+
 function App() {
   return (
-    <button onClick={() => Sentry.captureMessage('Clicou no botão')}>Break the world</button>
+    <button onClick={() => p.methodDoesNotExist()}>Botão Quebrado</button>
   );
 }
 
